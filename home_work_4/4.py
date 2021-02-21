76f5b2cdf93c9081199069684f18924da688d875
Niolai <klark.charlz@mail.ru>
2021-02-21 19:16:08 +0300

    По ошибке удалил старые задания. Возвращаю.

--- home_work_4/4.py
+++ home_work_4/4.py
@@ -1,80 +1,39 @@
 """
 Задание 4.
-Приведены два алгоритма. В них определяется число,
-которое встречается в массиве чаще всего.
-Сделайте профилировку каждого алгоритма через timeit
-Попытайтесь написать третью версию, которая будет самой быстрой.
-Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
-Без аналитики задание считается не принятым
+Реализуйте скрипт "Кэширование веб-страниц"
+Функция должна принимать url-адрес и проверять
+есть ли в кэше соответствующая страница, если нет, то вносит ее в кэш
+Подсказка: задачу решите обязательно с применением 'соленого' хеширования и хеш-таблиц
+Можете условжнить задачу, реализовав ее через ООП
+Не забудьте, что кэширование - механизм, а хеш-таблица - средство его реализации
 """
-from timeit import Timer
-
-array = [1, 3, 1, 3, 4, 5, 1]*100
-
-
-def func_1():
-    m = 0
-    num = 0
-    for i in array:
-        count = array.count(i)
-        if count > m:
-            m = count
-            num = i
-    return f'Чаще всего встречается число {num}, ' \
-           f'оно появилось в массиве {m} раз(а)'
+from sys import argv
+from hashlib import md5
+import json
+import os.path
+from pathlib import Path
 
 
-def func_2():
-    new_array = []
-    for el in array:
-        count2 = array.count(el)
-        new_array.append(count2)
-
-    max_2 = max(new_array)
-    elem = array[new_array.index(max_2)]
-    return f'Чаще всего встречается число {elem}, ' \
-           f'оно появилось в массиве {max_2} раз(а)'
-
-
-print(func_1())
-print(func_2())
-
-t1 = Timer("func_1()", globals=globals())
-print("Version 1", t1.timeit(number=1_000), "seconds")
-t2 = Timer("func_2()", globals=globals())
-print("Version 2", t2.timeit(number=1_000), "seconds")
-
-"""
-Версия 2 показала гораздо более худший результат.
-
-Первый вариант плох тем что мы проверяем количество вхождений в массив каждого эллемента,
-даже  с учетом того что подобный элемент нам уже попадался и мы проводили для него аналогичную операцию,
-что лишняя трата времени.
-
-Во втором варианте проводится слишком много операций,
-count, append, max, index что отнимает много времени в сумме.
-"""
+def hash_urls():
+    if os.path.exists('hash.txt'):  # проверяем существует ли файл для хранения данных
+        print(f"Принятый url: {argv[1]}")
+        salt = argv[1].split('.')[1]  # будем использовать вот такую соль
+        hashing = md5(salt.encode('utf-8') + argv[1].encode('utf-8')).hexdigest()
+        with open('hash.txt', 'r') as f:
+            data = f.read()
+            try:
+                data = json.loads(data)
+            except json.decoder.JSONDecodeError:
+                data = {}
+            if hashing in data:
+                print(f"По данному хешу содержится: {data[hashing]}")
+            else:
+                data[hashing] = argv[1]
+            with open('hash.txt', 'w') as file:
+                file.write(json.dumps(data))
+    else:  # если не существует создаем его и заново вызываем функцию
+        Path('hash.txt').touch()
+        hash_urls()
 
 
-# написал и проверил свою версию
-def func_3():
-    max_num = array[0]
-    max_count = array.count(max_num)
-    for elem in set(array):
-        cnt = array.count(elem)
-        if cnt > max_count:
-            max_count = cnt
-            max_num = elem
-    return f'Чаще всего встречается число {max_num}, ' \
-           f'оно появилось в массиве {max_count} раз(а)'
-
-
-print(func_3())
-t3 = Timer("func_3()", globals=globals())
-print("Version 3", t3.timeit(number=1_000), "seconds")
-
-"""
-Эта версия оказалась гораздо быстрее первых двух, впринципе она похожа на первую,
-только мы не подсчитываем количество вхождений в массив тех эллементов которые нам уже попадались,
-что сильно экономит время.
-"""
+hash_urls()
